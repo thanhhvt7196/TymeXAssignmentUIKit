b7d99a2daf00bdb4d067e75ca71bfa5b5e0b6a06
Vũ Tiến Thành Hà <thanhhvt7196@gmail.com>
2025-06-04 07:46:34 +0700

    Initial Commit

---
+++ TymeXAssignmentUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TymeXAssignmentUIKit
+//
+//  Created by thanh tien on 4/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TymeXAssignmentUIKitTests/TymeXAssignmentUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TymeXAssignmentUIKitTests.swift
+//  TymeXAssignmentUIKitTests
+//
+//  Created by thanh tien on 4/6/25.
+//
+
+import Testing
+@testable import TymeXAssignmentUIKit
+
+struct TymeXAssignmentUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
