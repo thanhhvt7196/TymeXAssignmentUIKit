54705417165f1f76a5568b48e933922052b429a1
Vũ Tiến Thành Hà <thanhhvt7196@gmail.com>
2025-06-05 11:01:21 +0700

    extension for RxRelay to transform relay to AnyObserver

--- TymeXAssignmentUIKit/Common/Protocols/ViewModel.swift
+++ TymeXAssignmentUIKit/Common/Protocols/ViewModel.swift
@@ -9,6 +9,16 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+protocol InputCompatible {
+    associatedtype InputCompatibleType: ViewModel
+    var inputs: Inputs<InputCompatibleType> { get }
+}
+
+protocol OutputCompatible {
+    associatedtype OutputCompatibleType
+    var outputs: Outputs<OutputCompatibleType> { get }
+}
+
 class ViewModel: NSObject {
     enum LifeCycle {
         case didAppear
@@ -18,15 +28,46 @@ class ViewModel: NSObject {
         case didDisappear
         case didLayoutSubviews
     }
-
-    // handle view controller's state in viewmodel
+    
     let lifeCycle = PublishRelay<LifeCycle>()
     let isLoading = BehaviorRelay<Bool>(value: false)
     let errorMessage = PublishRelay<String>()
-
+    
     let disposeBag = DisposeBag()
     
     deinit {
         print("\(String(describing: self)) deinit")
     }
+}
+
+class Inputs<Base>: ReactiveCompatible {
+    let vm: Base
+
+    init(_ vm: Base) {
+        self.vm = vm
+    }
+}
+
+extension ViewModel: InputCompatible {
+    typealias InputCompatibleType = ViewModel
+}
+
+extension InputCompatible where Self: ViewModel {
+    var inputs: Inputs<Self> { return Inputs(self) }
+}
+
+class Outputs<Base>: ReactiveCompatible {
+    let vm: Base
+
+    init(_ vm: Base) {
+        self.vm = vm
+    }
+}
+
+extension ViewModel: OutputCompatible {
+    typealias OutputCompatibleType = ViewModel
+}
+
+extension OutputCompatible {
+    var outputs: Outputs<Self> { return Outputs(self) }
 }
---
+++ TymeXAssignmentUIKit/Common/Utils/Observable+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Observable+Extensions.swift
+//  TymeXAssignmentUIKit
+//
+//  Created by thanh tien on 5/6/25.
+//
+
+import RxSwift
+import RxCocoa
+
+extension PublishRelay {
+    func asObserver() -> AnyObserver<Element> {
+        return .init { [weak self] event in
+            guard let element = event.element else {
+                return
+            }
+            self?.accept(element)
+        }
+    }
+}
+
+extension BehaviorRelay {
+    func asObserver() -> AnyObserver<Element> {
+        return .init { [weak self] event in
+            guard let element = event.element else {
+                return
+            }
+            self?.accept(element)
+        }
+    }
+}
+
+extension AnyObserver {
+    typealias NextHandler = (Element) -> Void
+
+    static func onNext(_ nextHandler: @escaping NextHandler) -> AnyObserver {
+        return AnyObserver(eventHandler: { event in
+            if case let .next(element) = event {
+                return nextHandler(element)
+            }
+        })
+    }
+}
