a98fc03a05125221ad06ac26e49cf30cf0b43ead
Vũ Tiến Thành Hà <thanhhvt7196@gmail.com>
2025-06-05 09:23:16 +0700

    Enable Network Logger with Pulse

--- TymeXAssignmentUIKit/Application/AppDelegate/AppDelegate.swift
+++ TymeXAssignmentUIKit/Application/AppDelegate/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Pulse
+import PulseProxy
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupPulse()
         return true
     }
 
@@ -31,6 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    private func setupPulse() {
+        NetworkLogger.enableProxy()
+    }
 }
 
